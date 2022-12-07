a2bdbbef0df71d5b0bddeff4621b066ef22c035f
Matheus-Nasci <matheus.nasci2002@hotmail.com>
2022-12-07 17:46:37 -0300

    teste

--- teste.sh
+++ teste.sh
@@ -2,25 +2,29 @@
 echo "Iniciando Aplicação Banco De Dados TotemSystem"
 
 sudo docker exec -it $(sudo docker ps -aq) mysql -u root -p -B -N -e "
-	use totembd
+	use totembd;
+
 	create table endereco (
 	idEndereco int primary key auto_increment,
 	cep char(8) not null,
 	numero int not null
-	)
+	);
+
 	create table estacao(
 	idEstacao int primary key auto_increment,
 	fkEndereco int,
 	nomeEstacao varchar(45),
 	foreign key (fkEndereco) references endereco(idEndereco)
-	)
+	);
+
 	create table empresa(
 	idEmpresa int primary key auto_increment,
 	fkEstacao int,
 	nomeEmpresa varchar(45),
 	cnpj char(14),
 	foreign key (fkEstacao) references estacao(idEstacao)
-	)
+	);
+
 	create table usuario(
 	idUsuario int auto_increment,
 	fkEmpresa int,
@@ -30,14 +34,16 @@ sudo docker exec -it $(sudo docker ps -aq) mysql -u root -p -B -N -e "
 	senha varchar(50),
 	tipoUsuario int,
 	foreign key (fkEmpresa) references empresa(idEmpresa)
-	)
+	);
+
 	create table totem (
 	idTotem int primary key auto_increment,
 	fkEstacao int,
 	marca varchar (45),
 	so varchar (45),
 	foreign key (fkEstacao) references estacao(idEstacao)
-	)
+	);
+
 	create table disco (
 	idDisco int auto_increment,
 	fkTotem int,
@@ -46,13 +52,15 @@ sudo docker exec -it $(sudo docker ps -aq) mysql -u root -p -B -N -e "
 	modelo varchar (45),
 	volumeTotal double,
 	foreign key (fkTotem) references totem(idTotem)
-	)
+	);
+
 	create table memoria (
 	idMemoria int primary key auto_increment,
 	fkTotem int,
 	memoriaTotal double,
 	foreign key (fkTotem) references totem(idTotem)
-	)
+	);
+
 	create table processador (
 	idProcessador int primary key auto_increment,
 	fkTotem int,
@@ -61,13 +69,15 @@ sudo docker exec -it $(sudo docker ps -aq) mysql -u root -p -B -N -e "
 	microArq varchar (10),
 	frequencia double,
 	foreign key (fkTotem) references totem(idTotem)
-	)
+	);
+
 	create table processo (
 	idProcesso int primary key auto_increment,
 	fkTotem int,
 	nome varchar(45),
 	foreign key (fkTotem) references totem(idTotem)
-	)
+	);
+
 	create table dado (
 	idDado int auto_increment,
 	fkTotem int,
@@ -79,7 +89,8 @@ sudo docker exec -it $(sudo docker ps -aq) mysql -u root -p -B -N -e "
 	memoriaUsoProcesso double,
 	processadorUsoProcesso double,
 	foreign key (fkTotem) references totem(idTotem)
-	)
+	);
+    
 	create table reporte (
 	idReporte int primary key auto_increment,
 	idTotemReporte varchar(45),
